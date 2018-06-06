1516cd6a5e9600cbbabddbcdc6eafb9cf2d136f3
kungreye <kungreye@sina.com>
2018-06-06 21:39:22 +0800

    db models & migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = Flask(__name__)   # __name__: predefined var, set to module name in which it it used.
+
+app = Flask(__name__)               # __name__: predefined var, set to module name in which it it used.
 app.config.from_object(Config)
+db = SQLAlchemy(app)                # engine created.
+migrate = Migrate(app, db)
 
-from app import routes  # bottom import; since routes(within app package) will import app variable defined here.
+
+from app import routes, models      # bottom import
