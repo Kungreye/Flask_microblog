fc6ae365ca358ab9a602a14790c9102912e79bfd
kungreye <kungreye@sina.com>
2018-06-09 00:17:57 +0800

    Chapter 6: Profile Page & Avatars

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from config import Config
 
 
 app = Flask(__name__)               # __name__: predefined var, set to module name in which it it used.
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)                # engine created.
 migrate = Migrate(app, db)
 
+login = LoginManager(app)
+login.login_view = 'login'  # force user to login by redirect to view func 'login'.
 
-from app import routes, models      # bottom import
+
+from app import routes, models      # bottom imports
